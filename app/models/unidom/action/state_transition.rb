db188043264949ff538217a69018495d7f986442
Topbit Du <topbit.du@gmail.com>
2016-10-09 12:46:35 +0800

    1, Improve the State Transition model to add the .transited_via scope, the .transited_by scope, the .subject_is scope, & the .caused_by scope.

--- app/models/unidom/action/state_transition.rb
+++ app/models/unidom/action/state_transition.rb
@@ -13,6 +13,11 @@ class Unidom::Action::StateTransition < Unidom::Action::ApplicationRecord
   belongs_to :subject,           polymorphic: true
   belongs_to :reason,            class_name:  'Unidom::Action::Reason'
 
+  scope :transited_via, ->(transitor_visitor) { where transitor_visitor: transitor_visitor }
+  scope :transited_by,  ->(transitor_party)   { where transitor_party:   transitor_party   }
+  scope :subject_is,    ->(subject)           { where subject:           subject           }
+  scope :caused_by,     ->(reason)            { where reason_id:         to_id(reason)     }
+
   def self.transit!(subject: nil, from_state: nil, thru_state: nil, transitor_visitor: nil, transitor_party: nil, reason: nil, opened_at: Time.now)
     create! transitor_visitor: transitor_visitor, transitor_party: transitor_party, subject: subject, reason: reason, from_state: from_state, thru_state: thru_state, opened_at: opened_at
   end
