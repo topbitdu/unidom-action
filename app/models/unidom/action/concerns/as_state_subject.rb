c8df2041079df95a4a44a044d89fc0b023cd81a8
Topbit Du <topbit.du@gmail.com>
2016-10-10 07:56:24 +0800

    1, Improve the As State Subject concern to add the #is_transited! method.

--- app/models/unidom/action/concerns/as_state_subject.rb
+++ app/models/unidom/action/concerns/as_state_subject.rb
@@ -6,6 +6,23 @@ module Unidom::Action::Concerns::AsStateSubject
 
     has_many :state_transitions, class_name: 'Unidom::Action::StateTransition', as: :subject
 
+    def is_transited!(from: nil, thru: nil, due_to: nil, via: nil, by: nil, at: Time.now)
+      state_transitions.create! from_state: from, thru_state: thru, transitor_visitor: via, transitor_party: by, reason: due_to, opened_at: at
+    end
+
+=begin
+    def is_transited?(from: nil, thru: nil, due_to: nil, via: nil, by: nil, at: Time.now)
+      query = state_transitions
+      query = query.from_transited_to from    if from.present?
+      query = query.thru_transited_to thru    if thru.present?
+      query = query.transited_by      by      if by.present?
+      query = query.transited_via     via     if via.present?
+      query = query.caused_by         due_to  if due_to.present?
+      query = query.valid_at          now: at if at.present?
+      query.exists?
+    end
+=end
+
   end
 
   module ClassMethods
