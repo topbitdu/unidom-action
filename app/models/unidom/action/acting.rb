d0d8a6b3242b9d2db880282f92441e86ff81e9a7
Topbit Du <topbit.du@gmail.com>
2016-10-12 12:36:09 +0800

    1, Improve the Acting model to add the .act! method.

--- app/models/unidom/action/acting.rb
+++ app/models/unidom/action/acting.rb
@@ -19,4 +19,8 @@ class Unidom::Action::Acting < Unidom::Action::ApplicationRecord
   scope :acted_is,  ->(acted)         { where acted:         acted         }
   scope :caused_by, ->(reason)        { where reason_id:     to_id(reason) }
 
+  def self.act!(it, from: nil, thru: nil, due_to: nil, by: nil, via: nil, at: Time.now, action_code: 'C')
+    create! from_value: from, thru_value: thru, actor_visitor: via, actor_party: by, acted: it, reason: due_to, action_code: action_code, opened_at: at
+  end
+
 end
