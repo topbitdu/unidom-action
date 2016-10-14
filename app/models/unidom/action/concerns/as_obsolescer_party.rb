46712bb2d89bf9efe095797d456ea24d265441d9
Topbit Du <topbit.du@gmail.com>
2016-10-14 15:09:24 +0800

    1, Improve the As Obsolescer Party concern to add the #obsolesce! method.

--- app/models/unidom/action/concerns/as_obsolescer_party.rb
+++ app/models/unidom/action/concerns/as_obsolescer_party.rb
@@ -6,6 +6,22 @@ module Unidom::Action::Concerns::AsObsolescerParty
 
     has_many :obsolesced_obsolescings, class_name: 'Unidom::Action::Obsolescing', as: :obsolescer_party
 
+    def obsolesce!(it, obsolescence_code: 'OBSL', due_to: nil, via: nil, at: Time.now)
+      obsolesced_obsolescings.create! obsolesced: it, obsolescence_code: 'OBSL', obsolescer_visitor: via, reason: due_to, opened_at: at
+    end
+
+=begin
+    def obsolesce?(it, obsolescence_code: 'OBSL', due_to: nil, via: nil, at: Time.now)
+      query = obsolesced_obsolescings
+      query = query.obsolesced_is         it                if it.present?
+      query = query.obsolesced_via        via               if via.present?
+      query = query.caused_by             due_to            if due_to.present?
+      query = query.obsolescence_coded_as obsolescence_code if obsolescence_code.present?
+      query = query.valid_at              now: at           if at.present?
+      query.alive.exists?
+    end
+=end
+
   end
 
   module ClassMethods
