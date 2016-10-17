8b5765edd1e1bb23f01b7ea279f1435ddffd4d29
Topbit Du <topbit.du@gmail.com>
2016-10-18 07:35:45 +0800

    1, Improve the As Actor Party concern to add the #act? method.

--- app/models/unidom/action/concerns/as_actor_party.rb
+++ app/models/unidom/action/concerns/as_actor_party.rb
@@ -10,7 +10,6 @@ module Unidom::Action::Concerns::AsActorParty
       acted_actings.create! acted: on, from_value: from, thru_value: thru, reason: due_to, actor_visitor: via, opened_at: at, action_code: action_code
     end
 
-=begin
     def act?(on: nil, due_to: nil, via: nil, at: Time.now, action_code: 'C')
       query = acted_actings
       query = query.acted_is        on          if on.present?
@@ -20,7 +19,6 @@ module Unidom::Action::Concerns::AsActorParty
       query = query.caused_by       due_to      if due_to.present?
       query.alive.exists?
     end
-=end
 
   end
 
