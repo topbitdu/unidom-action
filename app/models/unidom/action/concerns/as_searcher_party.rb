160e93116240279f5918542549e5cfc9fbeca85f
Topbit Du <topbit.du@gmail.com>
2016-12-27 10:54:42 +0800

    1, Improve the As Searcher Party concern to add the #search! method.

--- app/models/unidom/action/concerns/as_searcher_party.rb
+++ app/models/unidom/action/concerns/as_searcher_party.rb
@@ -6,6 +6,24 @@ module Unidom::Action::Concerns::AsSearcherParty
 
     has_many :searched_searchings, class_name: 'Unidom::Action::Searching', as: :searcher_party
 
+    def search!(it, on: '', version: '1', per: {}, due_to: nil, via: nil, at: Time.now, displaying: 0, of_total: 0, on_current_page: 0, of_total_page: 0, per_page: 0)
+      searched_searchings.create! resource_name: it, platform_name: on, platform_version: version, criteria: per,
+        reason: due_to, searcher_visitor: via, opened_at: at,
+        shown_count: displaying, found_count: of_total, current_page: on_current_page, total_pages: of_total_page, per_page: per_page
+    end
+
+=begin
+    def search?(it, on: '', version: '1', due_to: nil, at: Time.now)
+      query = searched_searchings
+      query = query.resource_name_is    it      if it.present?
+      query = query.platform_name_is    on      if on.present?
+      query = query.platform_version_is version if version.present?
+      query = query.caused_by           due_to  if due_to.present?
+      query = query.valid_at            now: at if at.present?
+      query.alive.exists?
+    end
+=end
+
   end
 
   module ClassMethods
