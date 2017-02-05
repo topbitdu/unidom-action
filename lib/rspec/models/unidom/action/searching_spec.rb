6136c97a6668680cc700dd51c7eaee10ea9f8822
Topbit Du <topbit.du@gmail.com>
2017-02-05 11:28:35 +0800

    1, Searching spec.

---
+++ lib/rspec/models/unidom/action/searching_spec.rb
@@ -0,0 +1,32 @@
+describe Unidom::Action::Searching, type: :model do
+
+  before :each do
+  end
+
+  after :each do
+  end
+
+  context do
+
+    model_attributes = {
+      searcher_visitor_id:   SecureRandom.uuid,
+      searcher_visitor_type: 'Unidom::Action::Searcher::Mock',
+      searcher_party_id:     SecureRandom.uuid,
+      searcher_party_type:   'Unidom::Action::SearcherParty::Mock',
+      reason_id:             SecureRandom.uuid,
+      resource_name:         'users',
+      platform_name:         'administration',
+      platform_version:      2,
+      criteria:              { name: 'admin' },
+      found_count:           63,
+      shown_count:           10,
+      per_page:              10,
+      total_pages:           7,
+      current_page:          2
+    }
+
+    it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
+
+  end
+
+end
