479c8e7a9dddf3b3af9fc5726f33be95a99646fa
Topbit Du <topbit.du@gmail.com>
2017-02-07 13:19:00 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/action/models_rspec.rb
@@ -0,0 +1,5 @@
+require 'rspec/models/unidom/action/reason_spec'           unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Action::Reason'
+require 'rspec/models/unidom/action/acting_spec'           unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Action::Acting'
+require 'rspec/models/unidom/action/state_transition_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Action::StateTransition'
+require 'rspec/models/unidom/action/obsolescing_spec'      unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Action::Obsolescing'
+require 'rspec/models/unidom/action/searching_spec'        unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Action::Searching'
