c587d64144a8334904418df2229e2ffde336d11b
Topbit Du <topbit.du@gmail.com>
2017-02-28 18:21:08 +0800

    1, Improve the Acting spec for the caused_by scope.

--- lib/rspec/models/unidom/action/acting_spec.rb
+++ lib/rspec/models/unidom/action/acting_spec.rb
@@ -27,6 +27,8 @@ describe Unidom::Action::Acting, type: :model do
 
     it_behaves_like 'belongs_to', model_attributes, :reason, Unidom::Action::Reason, { name: 'Some Reason' }
 
+    it_behaves_like 'monomorphic scope', model_attributes, :caused_by, :reason
+
   end
 
 end
