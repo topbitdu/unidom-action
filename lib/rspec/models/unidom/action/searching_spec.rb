504845c6eaa20239a05a88a0a6c21961b0bc9997
Topbit Du <topbit.du@gmail.com>
2017-03-06 22:28:56 +0800

    1, Improve the Searching spec for the validations on the #resource_name attribute, the #platform_name attribute, & the #platform_version attribute.

--- lib/rspec/models/unidom/action/searching_spec.rb
+++ lib/rspec/models/unidom/action/searching_spec.rb
@@ -25,11 +25,70 @@ describe Unidom::Action::Searching, type: :model do
       current_page:          2
     }
 
+    resource_name_max_length = described_class.columns_hash['resource_name'].limit
+    platform_name_max_length = described_class.columns_hash['platform_name'].limit
+
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
     it_behaves_like 'belongs_to', model_attributes, :reason, Unidom::Action::Reason, { name: 'Some Reason' }
 
     it_behaves_like 'monomorphic scope', model_attributes, :caused_by, :reason
+
+    it_behaves_like 'validates', model_attributes, :resource_name,
+      {                      } => 0,
+      { resource_name: nil   } => 1,
+      { resource_name: ''    } => 1,
+      { resource_name: '1'   } => 0,
+      { resource_name: 'A'   } => 0,
+      { resource_name: '11'  } => 0,
+      { resource_name: 'AA'  } => 0,
+      { resource_name: '111' } => 0,
+      { resource_name: 'AAA' } => 0,
+      { resource_name: '1'*(resource_name_max_length-1) } => 0,
+      { resource_name: 'A'*(resource_name_max_length-1) } => 0,
+      { resource_name: '1'*resource_name_max_length     } => 0,
+      { resource_name: 'A'*resource_name_max_length     } => 0,
+      { resource_name: '1'*(resource_name_max_length+1) } => 1,
+      { resource_name: 'A'*(resource_name_max_length+1) } => 1
+
+    it_behaves_like 'validates', model_attributes, :platform_name,
+      {                      } => 0,
+      { platform_name: nil   } => 1,
+      { platform_name: ''    } => 1,
+      { platform_name: '1'   } => 0,
+      { platform_name: 'A'   } => 0,
+      { platform_name: '11'  } => 0,
+      { platform_name: 'AA'  } => 0,
+      { platform_name: '111' } => 0,
+      { platform_name: 'AAA' } => 0,
+      { platform_name: '1'*(platform_name_max_length-1) } => 0,
+      { platform_name: 'A'*(platform_name_max_length-1) } => 0,
+      { platform_name: '1'*platform_name_max_length     } => 0,
+      { platform_name: 'A'*platform_name_max_length     } => 0,
+      { platform_name: '1'*(platform_name_max_length+1) } => 1,
+      { platform_name: 'A'*(platform_name_max_length+1) } => 1
+
+    it_behaves_like 'validates', model_attributes, :platform_version,
+      {                                   } => 0,
+      { platform_version: nil             } => 2,
+      { platform_version: ''              } => 2,
+      { platform_version: '1'             } => 0,
+      { platform_version: 1               } => 0,
+      { platform_version: 'A'             } => 1,
+      { platform_version: '1.23'          } => 1,
+      { platform_version: 1.23            } => 1,
+      { platform_version: '-0.01'         } => 1,
+      { platform_version: -0.01           } => 1,
+      { platform_version: '0'             } => 0,
+      { platform_version: 0               } => 0,
+      { platform_version: '0.01'          } => 1,
+      { platform_version: 0.01            } => 1,
+      { platform_version: '999999999'     } => 0,
+      { platform_version: 999_999_999     } => 0,
+      { platform_version: '1000000000'    } => 0,
+      { platform_version: 1_000_000_000   } => 0,
+      { platform_version: '1_000_000_001' } => 1,
+      { platform_version: 1_000_000_001   } => 1
 
   end
 
