ed0dad9916ea79f8eee7e789c274d29333ffc393
Topbit Du <topbit.du@gmail.com>
2017-03-22 07:29:35 +0800

    1, Improve the Acting model to include the As Caused concern.

--- app/models/unidom/action/acting.rb
+++ app/models/unidom/action/acting.rb
@@ -10,16 +10,17 @@ class Unidom::Action::Acting < Unidom::Action::ApplicationRecord
 
   include Unidom::Common::Concerns::ModelExtension
   include ProgneTapera::EnumCode
+  include Unidom::Action::Concerns::AsCaused
 
   belongs_to :actor_visitor, polymorphic: true
   belongs_to :actor_party,   polymorphic: true
   belongs_to :acted,         polymorphic: true
-  belongs_to :reason,        class_name:  'Unidom::Action::Reason'
+  #belongs_to :reason,        class_name:  'Unidom::Action::Reason'
 
   scope :acted_via, ->(actor_visitor) { where actor_visitor: actor_visitor }
   scope :acted_by,  ->(actor_party)   { where actor_party:   actor_party   }
   scope :acted_is,  ->(acted)         { where acted:         acted         }
-  scope :caused_by, ->(reason)        { where reason_id:     to_id(reason) }
+  #scope :caused_by, ->(reason)        { where reason_id:     to_id(reason) }
 
   code :action, Unidom::Action::Action
 
