769960056e0dab0cb405e6e23e36433b1189e3b3
Topbit Du <topbit.du@gmail.com>
2017-03-24 20:03:25 +0800

    1, Improve the State Transition model to include the As Caused concern.

--- app/models/unidom/action/state_transition.rb
+++ app/models/unidom/action/state_transition.rb
@@ -8,16 +8,15 @@ class Unidom::Action::StateTransition < Unidom::Action::ApplicationRecord
   self.table_name = 'unidom_state_transitions'
 
   include Unidom::Common::Concerns::ModelExtension
+  include Unidom::Action::Concerns::AsCaused
 
   belongs_to :transitor_visitor, polymorphic: true
   belongs_to :transitor_party,   polymorphic: true
   belongs_to :subject,           polymorphic: true
-  belongs_to :reason,            class_name:  'Unidom::Action::Reason'
 
   scope :transited_via, ->(transitor_visitor) { where transitor_visitor: transitor_visitor }
   scope :transited_by,  ->(transitor_party)   { where transitor_party:   transitor_party   }
   scope :subject_is,    ->(subject)           { where subject:           subject           }
-  scope :caused_by,     ->(reason)            { where reason_id:         to_id(reason)     }
 
   def self.transit!(subject: nil, from_state: nil, thru_state: nil, transitor_visitor: nil, transitor_party: nil, reason: nil, opened_at: Time.now)
     create! transitor_visitor: transitor_visitor, transitor_party: transitor_party, subject: subject, reason: reason, from_state: from_state, thru_state: thru_state, opened_at: opened_at
