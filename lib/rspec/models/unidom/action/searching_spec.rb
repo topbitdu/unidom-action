749c2cf42c3b663c4a1b34909c24191f06f854da
Topbit Du <topbit.du@gmail.com>
2017-03-26 21:43:53 +0800

    1, Improve the Searching spec for the As Caused concern.

--- lib/rspec/models/unidom/action/searching_spec.rb
+++ lib/rspec/models/unidom/action/searching_spec.rb
@@ -26,10 +26,7 @@ describe Unidom::Action::Searching, type: :model do
     }
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
-
-    it_behaves_like 'belongs_to', model_attributes, :reason, Unidom::Action::Reason, { name: 'Some Reason' }
-
-    it_behaves_like 'monomorphic scope', model_attributes, :caused_by, :reason
+    it_behaves_like 'Unidom::Action::Concerns::AsCaused',       model_attributes
 
     it_behaves_like 'validates text', model_attributes, :resource_name,
       length: 1..described_class.columns_hash['resource_name'].limit
