2c8ba967d27afa3676721155cf6493c042032384
Topbit Du <topbit.du@gmail.com>
2017-04-02 21:41:44 +0800

    1, Improve the RSpec shared examples to require the As Acted shared examples, & the As Actor Party shared examples.

--- lib/unidom/action/rspec_shared_examples.rb
+++ lib/unidom/action/rspec_shared_examples.rb
@@ -1,1 +1,3 @@
+require 'rspec/models/unidom/action/concerns/as_acted_shared_examples'
+require 'rspec/models/unidom/action/concerns/as_actor_party_shared_examples'
 require 'rspec/models/unidom/action/concerns/as_caused_shared_examples'
