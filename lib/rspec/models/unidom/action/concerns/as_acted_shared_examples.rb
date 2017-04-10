bedf5f3d8dec9eae9fdbb251d403cea6d146fee5
Topbit Du <topbit.du@gmail.com>
2017-04-10 17:00:20 +0800

    1, Improve the As Acted shared examples for the coding style.

--- lib/rspec/models/unidom/action/concerns/as_acted_shared_examples.rb
+++ lib/rspec/models/unidom/action/concerns/as_acted_shared_examples.rb
@@ -1,21 +1,25 @@
 shared_examples 'Unidom::Action::Concerns::AsActed' do |model_attributes|
 
-  acting_1_attributes = {
-    actor_visitor_id:   SecureRandom.uuid,
-    actor_visitor_type: 'Unidom::Action::ActorVisitor::Mock',
-    actor_party_id:     SecureRandom.uuid,
-    actor_party_type:   'Unidom::Action::ActorParty::Mock',
-    reason_id:          SecureRandom.uuid
-  }
+  context do
 
-  acting_2_attributes = {
-    actor_visitor_id:   SecureRandom.uuid,
-    actor_visitor_type: 'Unidom::Action::ActorVisitor::Mock',
-    actor_party_id:     SecureRandom.uuid,
-    actor_party_type:   'Unidom::Action::ActorParty::Mock',
-    reason_id:          SecureRandom.uuid
-  }
+    acting_1_attributes = {
+      actor_visitor_id:   SecureRandom.uuid,
+      actor_visitor_type: 'Unidom::Action::ActorVisitor::Mock',
+      actor_party_id:     SecureRandom.uuid,
+      actor_party_type:   'Unidom::Action::ActorParty::Mock',
+      reason_id:          SecureRandom.uuid
+    }
 
-  it_behaves_like 'has_many', model_attributes, :actings, Unidom::Action::Acting, [ acting_1_attributes, acting_2_attributes ]
+    acting_2_attributes = {
+      actor_visitor_id:   SecureRandom.uuid,
+      actor_visitor_type: 'Unidom::Action::ActorVisitor::Mock',
+      actor_party_id:     SecureRandom.uuid,
+      actor_party_type:   'Unidom::Action::ActorParty::Mock',
+      reason_id:          SecureRandom.uuid
+    }
+
+    it_behaves_like 'has_many', model_attributes, :actings, Unidom::Action::Acting, [ acting_1_attributes, acting_2_attributes ]
+
+  end
 
 end
