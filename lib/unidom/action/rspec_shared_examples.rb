d2b8697b8836abd686b399df5740a2868cd8c758
Topbit Du <topbit.du@gmail.com>
2017-04-10 16:59:07 +0800

    1, Improve the RSpec shared examples manifest to require the As Obsolesced shared examples.

--- lib/unidom/action/rspec_shared_examples.rb
+++ lib/unidom/action/rspec_shared_examples.rb
@@ -1,3 +1,4 @@
 require 'rspec/models/unidom/action/concerns/as_acted_shared_examples'
 require 'rspec/models/unidom/action/concerns/as_actor_party_shared_examples'
 require 'rspec/models/unidom/action/concerns/as_caused_shared_examples'
+require 'rspec/models/unidom/action/concerns/as_obsolesced_shared_examples'
