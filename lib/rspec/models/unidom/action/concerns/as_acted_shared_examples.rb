682213b8abfa6fe3be91ef1515a2adca7abad70b
Topbit Du <topbit.du@gmail.com>
2017-04-26 09:32:31 +0800

    1, Improve the As Acted shared examples for the argument validations of the #is_acted! method.

--- lib/rspec/models/unidom/action/concerns/as_acted_shared_examples.rb
+++ lib/rspec/models/unidom/action/concerns/as_acted_shared_examples.rb
@@ -1,4 +1,4 @@
-shared_examples 'Unidom::Action::Concerns::AsActed' do |model_attributes|
+shared_examples 'Unidom::Action::Concerns::AsActed' do |model_attributes, actor_party, actor_visitor|
 
   context do
 
@@ -19,6 +19,11 @@ shared_examples 'Unidom::Action::Concerns::AsActed' do |model_attributes|
     }
 
     it_behaves_like 'has_many', model_attributes, :actings, Unidom::Action::Acting, [ acting_1_attributes, acting_2_attributes ]
+
+    model = described_class.create! model_attributes
+    it_behaves_like 'assert_present!', model, :is_acted!, [ { from: nil,                  thru: nil,                  due_to: nil, by: actor_party, via: actor_visitor, at: Time.now, action_code: 'C' } ], [ :by, :via, :at, :action_code ]
+    it_behaves_like 'assert_present!', model, :is_acted!, [ { from: {},                   thru: {},                   due_to: nil, by: actor_party, via: actor_visitor, at: Time.now, action_code: 'C' } ], [ :by, :via, :at, :action_code ]
+    it_behaves_like 'assert_present!', model, :is_acted!, [ { from: { key_1: 'value_1' }, thru: { key_1: 'value_2' }, due_to: nil, by: actor_party, via: actor_visitor, at: Time.now, action_code: 'C' } ], [ :by, :via, :at, :action_code ]
 
   end
 
